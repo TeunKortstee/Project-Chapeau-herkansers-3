a46cc9656923d1ae4d321c3afa5c0db16ba230c1
Luciano010 <693538@student.inholland.nl>
2024-05-15 13:37:36 +0200

    Versie 1 Design

---
+++ Service/MenuItemService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+
+namespace Service
+{
+    public class MenuItemService
+    {
+        private MenuItemDao menuItemDao;
+
+        public MenuItemService()
+        {
+            menuItemDao = new MenuItemDao();
+        }
+
+
+    }
+}
