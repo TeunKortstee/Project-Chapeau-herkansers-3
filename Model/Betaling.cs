97469a90a8cbd53de2bc79361e3000139cba0ffe
LawrenceDeveloper <113107075+LawrenceDeveloper@users.noreply.github.com>
2024-06-05 10:55:51 +0200

    Betaling

---
+++ Model/Betaling.cs
@@ -0,0 +1,19 @@
+﻿namespace Model
+{
+    public class Betaling
+    {
+        public int BetalingId { get; set; }
+        public int Methode { get; set; }
+        public double Bedrag { get; set; }
+        public int RekeningId { get; set; }
+
+        public Betaling(int _BetalingId, int _Methode, double _Bedrag, int _RekeningId) {
+            BetalingId = _BetalingId;
+            Methode = _Methode;
+            Bedrag = _Bedrag;
+            RekeningId = _RekeningId;
+          
+        
+        }
+    }
+}
