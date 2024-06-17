854eb5a33d1f5646eac9a298941e299592e9e7bd
Luciano010 <693538@student.inholland.nl>
2024-06-17 15:35:13 +0200

    Versie 32

--- DAL/RekeningDao.cs
+++ DAL/RekeningDao.cs
@@ -62,8 +62,7 @@ namespace DAL
             return rekeningen;
         }
 
-        public void RekeningBetaald(Rekening rekening)
-        {
+        public void RekeningBetaald(Rekening rekening) {
             string query = "UPDATE Rekeningen SET Betaald = 1, Datum = GETDATE() WHERE RekeningId = @ID; UPDATE Bestellingen SET Betaald = 1 WHERE TableNr = @TableNr AND Betaald = 0";
 
 
