85eb25a547351fb4c5e34258df918089f93ef6f0
TeunKortstee <teunkortstee@gmail.com>
2024-06-17 13:08:20 +0200

    blip

--- Project-Chapeau herkansers 3/TafelBestellingUserControl.Designer.cs
+++ Project-Chapeau herkansers 3/TafelBestellingUserControl.Designer.cs
@@ -28,17 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
+            comboBox1 = new ComboBox();
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            richTextBox1 = new RichTextBox();
+            label2 = new Label();
+            button3 = new Button();
             SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(94, 120);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(182, 33);
+            comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(41, 336);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(214, 336);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(57, 180);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 25);
+            label1.TabIndex = 3;
+            label1.Text = "label1";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(41, 425);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(285, 168);
+            richTextBox1.TabIndex = 4;
+            richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(41, 397);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 25);
+            label2.TabIndex = 5;
+            label2.Text = "label2";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(25, 50);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 34);
+            button3.TabIndex = 6;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
             // 
             // TafelBestellingUserControl
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(button3);
+            Controls.Add(label2);
+            Controls.Add(richTextBox1);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(comboBox1);
             Name = "TafelBestellingUserControl";
-            Size = new Size(320, 565);
+            Size = new Size(373, 722);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ComboBox comboBox1;
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private RichTextBox richTextBox1;
+        private Label label2;
+        private Button button3;
     }
 }
