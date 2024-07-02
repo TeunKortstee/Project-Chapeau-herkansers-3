7c69fe1853a400879f37711f1fcc30e07441dbb4
Lucas Cheung <701340@student.inholland.nl>
2024-07-02 09:17:51 +0200

    verandering startMenu

--- Project-Chapeau herkansers 3/UserControls/UserControlManager.cs
+++ Project-Chapeau herkansers 3/UserControls/UserControlManager.cs
@@ -5,7 +5,7 @@ namespace Project_Chapeau_herkansers_3.UserControls
     public partial class UserControlManager : UserControl
     {
         private const Functie startFunctie = Functie.Serveerder;
-        private const MenuType startMenu = MenuType.Drank;
+        private const MenuType startMenu = MenuType.Lunch;
 
         private Form1 form;
 
